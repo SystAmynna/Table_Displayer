392afb59cd325bef10864d180fc58984a1009780
SystAmynna <thelouisien@gmail.com>
2024-10-23 23:28:03 +0200

    🚀 Redfinition du test

--- src/test/java/tests/TestTDisplay.java
+++ src/test/java/tests/TestTDisplay.java
@@ -3,6 +3,8 @@ package tests;
 import main.TDisplay;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 public class TestTDisplay {
 
     @Test
@@ -20,6 +22,16 @@ public class TestTDisplay {
 
         TDisplay display = new TDisplay(table);
         display.display(true);
+        display.setTitle("Test TDisplay");
+
+        display.CONFIG.setCaseSize(15);
+        display.CONFIG.setDisplayValues(false);
+        display.CONFIG.setDefaultColor(Color.BLUE);
+        display.CONFIG.setDefaultTextColor(Color.YELLOW);
+        display.CONFIG.addColor(6, Color.GREEN);
+        display.CONFIG.addTextColor(6, Color.RED);
+
+
 
         try {
             Thread.sleep(2000);
@@ -31,7 +43,24 @@ public class TestTDisplay {
         display.fillZone(1,1, S-2, S-2, 1);
 
         try {
-            Thread.sleep(5000);
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        display.fillLine(1, -1);
+        display.fillColumn(18, -1);
+        display.fillZone(new Rectangle(8, 8, 10, 15), -1);
+        display.set(S-1, S-1, 0);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        display.randomize();
+        try {
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
